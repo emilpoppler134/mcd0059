a0f3a72e1fd3289ebc09f5a6f6577333c1b3452c
Emil Poppler <emilpoppler@Emils-MacBook-Air.local>
2023-04-04 11:03:31 +0200

    cc-exp keyup

--- private/js/index.js
+++ private/js/index.js
@@ -95,6 +95,7 @@ function showAddCardContent() {
       </div>
     </div>
   `;
+  document.querySelector(".checkoutInput[name=cc_exp]").addEventListener("keyup", handleExpKeyup);
   document.querySelector(".back-button").addEventListener("click", showStartContent);
   document.querySelector(".SubmitButton").addEventListener("click", handleAddCard);
 }
@@ -193,4 +194,31 @@ async function handlePay() {
   }
 
   console.log(status, data);
+}
+
+
+/* Keyup event listener */
+function handleExpKeyup(event) {
+  const code = event.keyCode;
+  const allowedKeys = [8];
+
+  if (allowedKeys.indexOf(code) !== -1) {
+    return;
+  }
+
+  event.target.value = event.target.value.replace(
+    /^([1-9]\/|[2-9])$/g, '0$1/' // 3 > 03/
+  ).replace(
+    /^(0[1-9]|1[0-2])$/g, '$1/' // 11 > 11/
+  ).replace(
+    /^([0-1])([3-9])$/g, '0$1/$2' // 13 > 01/3
+  ).replace(
+    /^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2' // 141 > 01/41
+  ).replace(
+    /^([0]+)\/|[0]+$/g, '0' // 0/ > 0 and 00 > 0
+  ).replace(
+    /[^\d\/]|^[\/]*$/g, '' // To allow only digits and `/`
+  ).replace(
+    /\/\//g, '/' // Prevent entering more than 1 `/`
+  );
 }
